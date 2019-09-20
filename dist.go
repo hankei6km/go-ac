28d052e137e3ebc88541fb49d0efed9d517caced
hankei6km <hankei6km@gmail.com>
2019-09-20 11:59:49 +0900

    Make Dist

---
+++ dist.go
@@ -0,0 +1,284 @@
+package ac
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+// Dist provide functions to write some CREDITS files for each binary files
+// (ie. foo_linux_386, foo_darwin_amd64 -> CREDITS_linxu_386, CREDITS_darwin_amd64)
+//
+// Dist は各バイナリファイルから、それぞれ用の CREDITS ファイルを書き出す機能を提供する.
+type Dist interface {
+	Run() error
+}
+
+// DistBuilder builds Dist.
+type DistBuilder interface {
+	GoSumFile(string) DistBuilder
+	WorkDir(string) DistBuilder
+	DistDir(string) DistBuilder
+	OutDir(string) DistBuilder
+	BaseName(string) DistBuilder
+	Uniq(bool) DistBuilder
+
+	OutputBuilder(OutputBuilder) DistBuilder
+
+	OutStream(io.Writer) DistBuilder
+	ErrStream(io.Writer) DistBuilder
+
+	Branch() DistBuilder
+	Build() Dist
+}
+
+type baseDistBuilder struct {
+	mu        *sync.Mutex
+	goSumFile string
+	workDir   string
+	distDir   string
+	outDir    string
+	baseName  string
+	uniq      bool
+
+	outputBuilder OutputBuilder
+
+	outStream io.Writer
+	errStream io.Writer
+}
+
+func (b *baseDistBuilder) GoSumFile(goSumFile string) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.goSumFile = goSumFile
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) WorkDir(workDir string) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.workDir = workDir
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) DistDir(distDir string) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.distDir = distDir
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) OutDir(outDir string) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.outDir = outDir
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) BaseName(baseName string) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.baseName = baseName
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) Uniq(uniq bool) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.uniq = uniq
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) OutputBuilder(outputBuilder OutputBuilder) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.outputBuilder = outputBuilder
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) OutStream(outStream io.Writer) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.outStream = outStream
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) ErrStream(errStream io.Writer) DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.errStream = errStream
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) branch() DistBuilder {
+	mu := b.mu
+	// mu.Lock() 呼び出し元で lock されているときだけ実行するように注意.
+	// defer mu.Unlock()
+
+	b.mu = nil
+	ret := *b // とりあえず
+	ret.mu = &sync.Mutex{}
+
+	b.mu = mu
+	return &ret
+}
+
+func (b *baseDistBuilder) Branch() DistBuilder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	return b.branch()
+}
+
+func (b *baseDistBuilder) Build() Dist {
+	return newBaseDist(b)
+}
+
+type outputHash struct {
+	outFileName string
+	hash        []byte
+}
+
+type baseDist struct {
+	goSumFile string
+	workDir   string
+	distDir   string
+	outDir    string
+	baseName  string
+	uniq      bool
+
+	outputBuilder OutputBuilder
+
+	outStream io.Writer
+	errStream io.Writer
+
+	builder DistBuilder
+
+	// hash []outputHash
+	hash []*outputHash
+}
+
+func (d *baseDist) output(distName string) error {
+	s := DistSuffix(distName)
+	// TODO: support replacement like as GoReleaser.
+	pOs := s[0]
+	pArch := s[1]
+
+	outFileName := filepath.Join(d.outDir, d.baseName+"_"+pOs+"_"+pArch)
+	out, err := os.Create(outFileName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	o := d.outputBuilder.Binary(filepath.Join(d.distDir, distName)).OutStream(out).Build()
+	hash, err := o.Flush()
+	d.hash = append(d.hash, &outputHash{
+		outFileName: outFileName,
+		hash:        hash,
+	})
+	return err
+}
+
+func (d *baseDist) uniqByHash() (uniqed bool, err error) {
+	l := len(d.hash)
+	t := d.hash[0]
+	p, _ := filepath.Split(t.outFileName)
+	dstFileName := filepath.Join(p, d.baseName)
+	for i := 1; i < l; i++ {
+		if bytes.Equal(t.hash, d.hash[i].hash) == false {
+			return false, nil
+		}
+	}
+	for i := 1; i < l; i++ {
+		if err := os.Remove(d.hash[i].outFileName); err != nil {
+			return false, err
+		}
+	}
+	if err := os.Rename(t.outFileName, dstFileName); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (d *baseDist) Run() error {
+	dirs, err := ioutil.ReadDir(d.distDir)
+	if err != nil {
+		return err
+	}
+	for _, p := range dirs {
+		if p.IsDir() {
+			// go.sum を上書きしているので、並列で動かさないように注意.
+			if err := d.output(p.Name()); err != nil {
+				return err
+			}
+		}
+	}
+	if len(d.hash) == 0 {
+		return fmt.Errorf("No %s file(s) has been created", d.baseName)
+	}
+	if d.uniq {
+		_, err := d.uniqByHash()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func newBaseDist(b *baseDistBuilder) *baseDist {
+	return &baseDist{
+		goSumFile: b.goSumFile,
+		workDir:   b.workDir,
+		distDir:   b.distDir,
+		outDir:    b.outDir,
+		baseName:  b.baseName,
+		uniq:      b.uniq,
+
+		outputBuilder: b.outputBuilder.Branch().
+			GoSumFile(b.goSumFile).
+			WorkDir(b.workDir),
+
+		outStream: b.outStream,
+		errStream: b.errStream,
+
+		builder: b.branch(),
+
+		hash: []*outputHash{},
+	}
+}
+
+// NewDistBuilder returns the instance of DistBuilder.
+func NewDistBuilder() DistBuilder {
+	return &baseDistBuilder{
+		mu:            &sync.Mutex{},
+		goSumFile:     "go.sum",
+		baseName:      "CREDITS",
+		uniq:          true,
+		outputBuilder: NewOutputBuilder(),
+		outStream:     os.Stdout,
+		errStream:     os.Stderr,
+	}
+}
---
+++ dist_test.go
@@ -0,0 +1,97 @@
+package ac
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_baseDist_Run(t *testing.T) {
+	cwd, err := os.Getwd()
+	assert.Nil(t, err, "check")
+	testDir := filepath.Join(cwd, "testdata")
+	distDir := filepath.Join(testDir, "distDir")
+	outDir := filepath.Join(testDir, "outDir")
+	cliDir := filepath.Join(testDir, "work_cli")
+	cliFile := filepath.Join(cliDir, "fake.sh")
+	workDir := filepath.Join(testDir, "work_dist")
+	goSumDir := filepath.Join(testDir, "goSum")
+	tests := []struct {
+		name           string
+		builder        DistBuilder
+		fakeScript     string
+		wantNumFOutput int
+		wantErr        bool
+	}{
+		{
+			name: "basic",
+			builder: NewDistBuilder().
+				GoSumFile(filepath.Join(goSumDir, "go.sum")).
+				DistDir(distDir).
+				OutDir(outDir).
+				WorkDir(workDir),
+			fakeScript: `#!/bin/sh
+# constant output.
+echo test
+`,
+			wantNumFOutput: 1,
+		}, {
+			name: "multiple",
+			builder: NewDistBuilder().
+				GoSumFile(filepath.Join(goSumDir, "go.sum")).
+				DistDir(distDir).
+				OutDir(outDir).
+				WorkDir(workDir),
+			fakeScript: `#!/bin/sh
+# different output.
+dd if=/dev/random count=5 status=none
+`,
+			wantNumFOutput: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ResetDir(workDir, os.ModePerm)
+			assert.Nil(t, err, "check")
+			defer os.RemoveAll(workDir)
+
+			err = ResetDir(outDir, os.ModePerm)
+			assert.Nil(t, err, "check")
+			defer os.RemoveAll(outDir)
+
+			err = ResetDir(cliDir, os.ModePerm)
+			assert.Nil(t, err, "check")
+			defer os.RemoveAll(cliDir)
+
+			err = func() error {
+				// ファイルを閉じないと実行できないので.
+				f, err := os.Create(cliFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer f.Close() // ファイルはディレクトリごと消されるので個別削除はしない.
+				_, err = io.Copy(f, strings.NewReader(tt.fakeScript))
+				return err
+			}()
+			assert.Nil(t, err, "check")
+			os.Chmod(cliFile, 0700)
+
+			d := tt.builder.
+				OutputBuilder(NewOutputBuilder().Cli(cliFile)).
+				Build()
+			err = d.Run()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("baseDist.Run() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			files, err := ioutil.ReadDir(outDir)
+			assert.Nil(t, err, "check")
+			assert.Equal(t, len(files), tt.wantNumFOutput, "Num of output files")
+		})
+	}
+}
